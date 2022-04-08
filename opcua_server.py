079e6b586f9fb83dcc3ea9af32afd04a2635857b
jicampos <jivcampos8@gmail.com>
2022-04-08 17:27:40 -0500

    adding arg parser for port number and ip address

--- opcua_server.py
+++ opcua_server.py
@@ -1,19 +1,24 @@
-# https://github.com/FreeOpcUa/opcua-asyncio/blob/master/examples/server-example.py
+import sys
+import argparse
 import logging
 import asyncio
-import sys
-sys.path.insert(0, "..")
 
 from asyncua import ua, Server
 from asyncua.common.methods import uamethod
 
 from serial_manager import SerialManager
 
+parser = argparse.ArgumentParser(description='Options for initializing server.')
+parser.add_argument('-p', '--port', help='Port number', type=str, default='4840')
+parser.add_argument('--ip', help='IP address', type=str, default='0.0.0.0')
+args = parser.parse_args()
+
 serial = SerialManager()
+
 
 @uamethod
 def send_command(parent, command):
-    return serial.send_command(command)    
+    return serial.send_command(command)
 
 
 async def main():
@@ -21,7 +26,7 @@ async def main():
     # setup our server
     server = Server()
     await server.init()
-    server.set_endpoint('opc.tcp://0.0.0.0:4840/freeopcua/server/')
+    server.set_endpoint(f'opc.tcp://{args.ip}:{args.port}') 
 
     # setup our own namespace, not really necessary but should as spec
     uri = 'http://examples.freeopcua.github.io'
@@ -36,7 +41,5 @@ async def main():
 
 
 if __name__ == '__main__':
-
     logging.basicConfig(level=logging.DEBUG)
-
     asyncio.run(main(), debug=True)
